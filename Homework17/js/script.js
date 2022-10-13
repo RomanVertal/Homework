b181cc15ac154ddd603ef6308c48d2f8f9bad09c
RomanVertal <rockerzlolz@gmail.com>
2022-10-13 14:33:25 +0300

    add HW 17

---
+++ Homework17/js/script.js
@@ -0,0 +1,32 @@
+let ageTable = document.querySelector('#age-table');
+
+console.log(ageTable)
+
+
+
+let label = document.querySelectorAll('#age-table label');
+
+console.log(label)
+
+
+
+let tdAge = document.querySelector('#age-table td');
+
+console.log(tdAge)
+
+
+
+let search = document.querySelector('form');
+
+console.log(search)
+
+
+
+let input1 = document.querySelector('form input');
+
+console.log(input1)
+
+
+let input2 = document.querySelector('form > input:last-Child');
+
+console.log(input2)
